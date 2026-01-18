4cb4d0cf71c5a7c195649e37baa84bcfc432bd40
lucianoz7 <lucianobarbosa6412@gmail.com>
2026-01-18 09:32:19 -0300

    Analisador de números

--- aula14/ex018/script.js
+++ aula14/ex018/script.js
@@ -21,8 +21,25 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('p')
+        item.innerHTML = `Valor ${num.value} adicionado`
+        list.appendChild(item)
+        res.innerHTML = ""
     } else {
         alert('Número inválido ou já encontrado na lista')
+    }
+     
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar')
+    } else {
+        let total = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
     }
 }
